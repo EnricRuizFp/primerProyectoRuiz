ab62c78a8d84ae67a9a03c0f9ca5b1eda34b6139
ERB 22 <enricruizfp@ibf.cat>
2024-09-17 18:42:33 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    echo "
+        <html>
+            <h1>EXPLICACION DE CREACION DE ENTORNOS</h1>
+            <p>
+            Primero añadimos los servidores virtuales al XAMPP:
+            RUTA: C:/xampp/apache/conf/extra
+            MODIFICACIONES:
+            <br>
+            <img src='virtualHosts PrimerRuiz.PNG'>
+
+            <br><br>
+            <img src='servidores virtuales.PNG'>
+        </html>
+    ";
+?>
