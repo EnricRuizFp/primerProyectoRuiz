5a1ef4dcc1818417cbfd456e85cb3db8d18c2bee
EnricRuizFp <EnricRuizFp>
2025-01-03 14:25:04 +0100

    subida la DB actualizada

--- DB/LaBuenizzima/LaBuenizzima_LOGS_DB.sql
+++ DB/LaBuenizzima/LaBuenizzima_LOGS_DB.sql
@@ -29,7 +29,7 @@ CREATE TABLE `LOGS_DB` (
   `tabla` varchar(50) DEFAULT NULL,
   `fecha` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `LOGS_DB` (
 
 LOCK TABLES `LOGS_DB` WRITE;
 /*!40000 ALTER TABLE `LOGS_DB` DISABLE KEYS */;
-INSERT INTO `LOGS_DB` VALUES (77,'create',NULL,'ofertas','2024-12-28 22:43:52'),(78,'delete',13,'ofertas','2024-12-28 22:44:17');
+INSERT INTO `LOGS_DB` VALUES (77,'create',NULL,'ofertas','2024-12-28 22:43:52'),(78,'delete',13,'ofertas','2024-12-28 22:44:17'),(79,'crear',NULL,'ingredientes','2025-01-01 13:16:49'),(80,'editar',33,'ingredientes','2025-01-01 13:17:14'),(81,'eliminar',33,'ingredientes','2025-01-01 13:17:26'),(82,'crear',NULL,'usuarios','2025-01-01 13:39:19'),(83,'crear',NULL,'usuarios','2025-01-01 13:39:32'),(84,'eliminar',19,'usuarios','2025-01-01 13:40:36'),(85,'crear',NULL,'usuarios','2025-01-01 13:40:51'),(86,'crear',NULL,'productos','2025-01-01 13:41:30'),(87,'eliminar',47,'productos','2025-01-01 13:41:36'),(88,'crear',NULL,'ofertas','2025-01-01 13:41:57'),(89,'eliminar',14,'ofertas','2025-01-01 13:42:01'),(90,'crear',NULL,'ingredientes','2025-01-02 20:50:27'),(91,'editar',26,'productos','2025-01-02 20:50:47'),(92,'editar',33,'productos','2025-01-02 20:51:20'),(93,'editar',33,'productos','2025-01-02 20:51:25'),(94,'editar',34,'productos','2025-01-02 20:51:52'),(95,'editar',33,'productos','2025-01-02 20:52:04');
 /*!40000 ALTER TABLE `LOGS_DB` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-28 23:46:26
+-- Dump completed on 2025-01-03 14:24:01
